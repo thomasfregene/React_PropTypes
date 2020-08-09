fb3ad470e644cc40ff365045848f5903ba860041
thomasfregene <fregzythomas@gmail.com>
2020-08-09 04:26:50 +0100

    Controlled Component -profile.js

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Profile from './Profile';
+
+class HomeComponent extends Component{
+
+    state ={
+        name: 'Francis',
+        lastName: 'Jones',
+        age: 23,
+        hobbies:['running', 'jumping'],
+        spanish: false,
+        message(){console.log('hello')},
+        car:{brand: 'Ford', model: 'Mustang'},
+        mother: 'Martha'
+    }
+    render(){
+        return(
+            <>
+                <Profile {...this.state}/>
+            </>
+
+        )
+    }
+}
+
+
+export default HomeComponent;
